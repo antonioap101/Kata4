bf7379697e7b09ae892ddb6e5485916d98b6f0f3
antonioap101 <121720588+antonioap101@users.noreply.github.com>
2024-01-03 21:12:12 +0000

    Finished Kata 4

--- src/main/java/software/ulpgc/kata4/Main.java
+++ src/main/java/software/ulpgc/kata4/Main.java
@@ -2,16 +2,20 @@ package software.ulpgc.kata4;
 
 
 import software.ulpgc.kata4.model.BMIHistogramCalculator;
-import software.ulpgc.kata4.model.CsvFilePersonLoader;
 import software.ulpgc.kata4.model.Person;
+import software.ulpgc.kata4.model.SQLitePersonLoader;
 import software.ulpgc.kata4.view.MainFrame;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Person> people = CsvFilePersonLoader.with("src/main/resources/bmi_dataset.csv").load();
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:sqlite:src/main/resources/bmi_data.db");
+        List<Person> people = SQLitePersonLoader.with(connection).load();
         Map<String, Integer> histogram = new BMIHistogramCalculator(people).calculate();
         MainFrame mainFrame = new MainFrame();
         mainFrame.getHistogramDisplay().show(histogram);
